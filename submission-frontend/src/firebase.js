8358e3bab5335bb28ae99fd037dbf3fca9e85c58
Albert Augustine <albertaugustine1884@gmail.com>
2025-09-11 23:10:25 +0530

    Update firebase.js

--- submission-frontend/src/firebase.js
+++ submission-frontend/src/firebase.js
@@ -1,16 +1,8 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
-const firebaseConfig = {
-    apiKey: "AIzaSyBOdS5onemFUTg8x4VBrVQSAjYuGNKOIc4",
-    authDomain: "water-depth.firebaseapp.com",
-    databaseURL: "https://water-depth-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "water-depth",
-    storageBucket: "water-depth.firebasestorage.app",
-    messagingSenderId: "567497131253",
-    appId: "1:567497131253:web:a0479941d5fbc75778b1bc",
-    measurementId: "G-Y84VZBJ168"
-};
+//Note: Get the firebase secret and import here
+const firebaseConfig = {};
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
